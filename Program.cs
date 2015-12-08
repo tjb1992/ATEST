e1c90b9181cb476bd10e4bfb3575f4469c45225d
tjb1992 <thomasjamesblack@yahoo.co.uk>
2015-12-08 10:44:02 +0000

    Hello World
    Hello, World

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Testing_Jenkins
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+        public static string CreateMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
